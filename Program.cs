79fd8bf48da7847ed4c0c3620d952f4fe908cb25
RhysDonlevy <rhysdonlevy1@gmail.com>
2025-03-10 18:22:39 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-namespace DungeonExplorer
+namespace DungeonGame
 {
-    internal class Program
+    //Initializes the Player Class
+    class Player
     {
-        static void Main(string[] args)
+        public string name;
+        public int health = 100;
+        public string item = "cloth garments";
+        public static string itemDef = "cloth garments";
+    }
+}
+   //Initializes the Room Class
+namespace DungeonGame
+{
+    class Room
+    {
+        public string Desc;
+        public string roomItem;
+    }
+}
+//Main Body Of Code
+namespace DungeonGame
+{
+    class Game
+    {
+        static void  Main(string[] args)
         {
-            Game game = new Game();
-            game.Start();
-            Console.WriteLine("Waiting for your Implementation");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            //Opening user prompt
+            Console.WriteLine("Welcome to the TEXT DUNGEON GAME");
+            Console.WriteLine("PLEASE ENTER YOUR NAME");
+            //Room 1 Attributes
+            Room room1 = new Room();
+            room1.Desc = "You are in a small room, the walls are covered in jagged cobblestone and the air is moist";
+            room1.roomItem = "WoodenSword ";
+            //Player name input and backstory
+            Player player1 = new Player();
+            string name = Console.ReadLine();
+            Console.WriteLine("Your Name is " + name + " You are an Adventurer from a far off land known as the Baklands");
+            Console.WriteLine("You have came to a secret Dungeon to the east in search of unimaginable riches ");
+
+            //Player Command List
+            bool play = true;
+            while (play)
+            {
+
+                Console.WriteLine("Enter a Command");
+                Console.WriteLine("!roomDesc");
+                Console.WriteLine("!roomItem");
+                Console.WriteLine("!playerDesc");
+                Console.WriteLine("!exitGame");
+                Console.WriteLine("!DefaultItem");
+                string command = Console.ReadLine();
+
+                if (command == "!roomDesc")
+                {
+                    Console.WriteLine(room1.Desc);
+                }
+                else if (command == "!roomItem")
+                {
+                    Console.WriteLine("in this room you found" + room1.roomItem);
+                    player1.item = room1.roomItem + player1.item;
+                }
+                else if (command == "!playerDesc")
+                {
+                    Console.WriteLine("your hp is " + player1.health + " and you have " + player1.item + " Equipped");
+                }
+                else if (command == "!exitGame")
+                {
+                    play = false;
+                    Console.WriteLine("Thanks for playing");
+                }
+                else if (command == "!DefaultItem")
+                {
+                    def();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command");
+                }
+                //Console.WriteLine(name);
+            }
+        }
+
+        private static void def()
+        {
+            Console.WriteLine("Your starting gear was "+Player.itemDef);    
         }
     }
 }
