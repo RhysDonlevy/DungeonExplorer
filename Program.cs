f7eaf41a24f69f5652d89d3b0b9f7c56bd6dd7b1
RhysDonlevy <rhysdonlevy1@gmail.com>
2025-03-13 05:31:30 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,92 +1,32 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace DungeonGame
+Console.WriteLine("Welcome to the Dungeon Text Adventure Game!");
+Console.WriteLine("");
+Console.WriteLine("Command List:");
+Console.WriteLine("!GetItem");
+Console.WriteLine("!CheckRoom");
+
+class Player
 {
-    //Initializes the Player Class
-    class Player
-    {
-        public string name;
-        public int health = 100;
-        public string item = "cloth garments";
-        public static string itemDef = "cloth garments";
-    }
+ public string Name;
+ int health;
+ public string Item;
+ public Player(string name, int health, string item){
+  Player character = new Player();
+  character.Name = "Jim";
+  character.health = 100;
+  character.Item = "Hammer"; 
+  Console.WriteLine(character.Name);
+ }
+
+ private Player()
+ {
+  throw new NotImplementedException();
+ }
 }
-   //Initializes the Room Class
-namespace DungeonGame
+
+class Room
 {
-    class Room
-    {
-        public string Desc;
-        public string roomItem;
-    }
-}
-//Main Body Of Code
-namespace DungeonGame
-{
-    class Game
-    {
-        static void  Main(string[] args)
-        {
-            //Opening user prompt
-            Console.WriteLine("Welcome to the TEXT DUNGEON GAME");
-            Console.WriteLine("PLEASE ENTER YOUR NAME");
-            //Room 1 Attributes
-            Room room1 = new Room();
-            room1.Desc = "You are in a small room, the walls are covered in jagged cobblestone and the air is moist";
-            room1.roomItem = "WoodenSword ";
-            //Player name input and backstory
-            Player player1 = new Player();
-            string name = Console.ReadLine();
-            Console.WriteLine("Your Name is " + name + " You are an Adventurer from a far off land known as the Baklands");
-            Console.WriteLine("You have came to a secret Dungeon to the east in search of unimaginable riches ");
-
-            //Player Command List
-            bool play = true;
-            while (play)
-            {
-
-                Console.WriteLine("Enter a Command");
-                Console.WriteLine("!roomDesc");
-                Console.WriteLine("!roomItem");
-                Console.WriteLine("!playerDesc");
-                Console.WriteLine("!exitGame");
-                Console.WriteLine("!DefaultItem");
-                string command = Console.ReadLine();
-
-                if (command == "!roomDesc")
-                {
-                    Console.WriteLine(room1.Desc);
-                }
-                else if (command == "!roomItem")
-                {
-                    Console.WriteLine("in this room you found" + room1.roomItem);
-                    player1.item = room1.roomItem + player1.item;
-                }
-                else if (command == "!playerDesc")
-                {
-                    Console.WriteLine("your hp is " + player1.health + " and you have " + player1.item + " Equipped");
-                }
-                else if (command == "!exitGame")
-                {
-                    play = false;
-                    Console.WriteLine("Thanks for playing");
-                }
-                else if (command == "!DefaultItem")
-                {
-                    def();
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Command");
-                }
-                //Console.WriteLine(name);
-            }
-        }
-
-        private static void def()
-        {
-            Console.WriteLine("Your starting gear was "+Player.itemDef);    
-        }
-    }
+ private string Desc;
+ private string Item;
 }
